ba26d2a6069ff1e060ee92a71309f2020598d7c8
Kingshuk Dasgupta <rextrebat@gmail.com>
2012-09-24 09:12:06 -0500

    Location Categories and Similarities table

--- hgweb/hgweb.py
+++ hgweb/hgweb.py
@@ -56,6 +56,7 @@ def show_hotels():
     hotels = []
     loc = int(request.args.get("loc"))
     sim = request.args.get("sim", "false")
+    loccat = request.args.get("loccat", "false")
     db = g.mongo.hotelgenome
     h_query = db.locations.find_one({"loc": loc}, {"query": 1})
     if h_query:
@@ -63,7 +64,8 @@ def show_hotels():
         h_query = 'db.hotels.find(' + h_query + ')'
         res = eval(h_query)
         hotels = [dict(name=h["name"], h_id=h["hotelId"]) for h in res]
-    return render_template('hotels.html', hotels=hotels, sim=sim)
+    return render_template('hotels.html',
+            hotels=hotels, sim=sim, loccat=loccat)
 
 
 @app.route('/genome')
@@ -105,29 +107,88 @@ def similar_hotels():
     cursor = g.db.cursor()
     cursor.execute(
             """
-            SELECT hotel_b, sim_score
-            FROM top_similar_hotels
-            WHERE hotel_a = %s
-            ORDER BY sim_score DESC
+            SELECT s.hotel_b, h.name, s.sim_score
+            FROM hotel_basic h, top_similar_hotels s
+            WHERE s.hotel_a = %s
+            AND h.hotel_id = s.hotel_b
+            ORDER BY s.sim_score DESC
             """, (comp_hotel)
             )
     res = cursor.fetchall()
-    ids = [r[0] for r in res]
-    hotels = [dict(h_id=r[0], score=r[1]) for r in res]
-    db = g.mongo.hotelgenome
-    res2 = db.hotels.find({"hotelId": {"$in": ids}})
-    names = [dict(h_id=r["hotelId"],name=r["name"]) for r in res2]
-    for i, h in enumerate(hotels):
-        for r in names:
-            if h["h_id"] == r["h_id"]:
-                hotels[i]["name"] = r["name"]
+    hotels = [dict(h_id=r[0], name=r[1], score=r[2]) for r in res]
+    ids_b = [r[0] for r in res if r[0] > comp_hotel]
+    ids_a = [r[0] for r in res if r[0] < comp_hotel]
+    in_b = ",".join(map(str, ids_b))
+    in_a = ",".join(map(str, ids_a))
+    cursor.execute(
+            """
+            SELECT hotel_b as h_id, sim_axis, sim_value
+            FROM hotel_similarity
+            WHERE hotel_a = %s
+            AND hotel_b IN (%s)
+            UNION
+            SELECT hotel_a as h_id, sim_axis, sim_value
+            FROM hotel_similarity
+            WHERE hotel_b = %s
+            AND hotel_a IN (%s)
+            """ % (comp_hotel, in_b, comp_hotel, in_a))
+    res = cursor.fetchall()
+    sim = {}
+    for r in res:
+        h_id, sim_axis, sim_value = r
+        if h_id not in sim:
+            sim[h_id] = {}
+        sim[h_id][sim_axis] = sim_value
+    cursor.execute(
+            """
+            SELECT DISTINCT category, sub_category
+            FROM genome_rules
+            """
+            )
+    res = cursor.fetchall()
+    axes=[dict(category=r[0], sub_category=r[1]) for r in res]
+    cursor.execute(
+            """
+            SELECT name FROM hotel_basic
+            WHERE hotel_id = %s
+            """ % comp_hotel)
+    comp_hotel_name = cursor.fetchone()[0]
     return render_template('similar_hotels.html',
-            hotels=hotels, comp_hotel=comp_hotel)
+            hotels=hotels,
+            sim=sim,
+            axes=axes,
+            comp_hotel=comp_hotel,
+            comp_hotel_name=comp_hotel_name
+            )
 
 
+@app.route('/loccat')
+def hotel_loc():
+    h_id = int(request.args.get("h_id"))
+    cursor = g.db.cursor()
+    cursor.execute(
+            """
+            SELECT category, count
+            FROM loc_categories
+            WHERE hotel_id = %s
+            """ % h_id
+            )
+    res = cursor.fetchall()
+    loccat = [dict(category=r[0], count=r[1]) for r in res]
+    return render_template('loccat.html', 
+            loccat=loccat)
 
-    
 
+    loc = int(request.args.get("loc"))
+    sim = request.args.get("sim", "false")
+    db = g.mongo.hotelgenome
+    h_query = db.locations.find_one({"loc": loc}, {"query": 1})
+    if h_query:
+        h_query = h_query["query"]
+        h_query = 'db.hotels.find(' + h_query + ')'
+        res = eval(h_query)
+        hotels = [dict(name=h["name"], h_id=h["hotelId"]) for h in res]
+    return render_template('hotels.html', hotels=hotels, sim=sim)
 
 
 if __name__ == '__main__':
