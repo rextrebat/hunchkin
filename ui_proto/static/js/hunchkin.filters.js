ca3b8dc9308a4c589e61c7a321ebf67a7081b198
Kingshuk Dasgupta <rextrebat@gmail.com>
2013-04-17 00:35:21 -0500

    Updated UI with Metro Admin Template look and feel

---
+++ ui_proto/static/js/hunchkin.filters.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* Filters */
+
+angular.module('hotelrecoFilters', []).filter('rateFilter', function() {
+});
+
+angular.module('hotelrecoFilters').filter('cut5', function() {
+});
+
